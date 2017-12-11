b0f532138e9da9f4d1943f277b8b0b6a30b748af
Zeradam-Fantaye <zeradam.fantaye@gmail.com>
2017-12-10 18:38:48 -0800

    Completed major functionality left with personalizing it

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,6 @@
 // Add an event handler Which handles the submit event from our <form>
 document.getElementById("issueInputForm").addEventListener("submit", saveIssue);
 
-/*function saveIssue(e) {
-  var issueDesc = document.getElementById('issueDescInput').value;
-  var issueSeverity = document.getElementById('issueSeverityInput').value;
-  var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
-  var issueId = chance.guid();
-  var issueStatus = 'Open';
-
-  var issue = {
-    id: issueId,
-    description: issueDesc,
-    severity: issueSeverity,
-    assignedTo: issueAssignedTo,
-    status: issueStatus
-  }
-
-  if (localStorage.getItem('issues') == null) {
-    var issues = [];
-    issues.push(issue);
-    localStorage.setItem('issues', JSON.stringify(issues));
-  } else {
-    var issues = JSON.parse(localStorage.getItem('issues'));
-    issues.push(issue);
-    localStorage.setItem('issues', JSON.stringify(issues));
-  }
-
-  document.getElementById('issueInputForm').reset();
-
-  fetchIssues();
-
-  e.preventDefault();
-}*/
-
 function saveIssue(e){
 	var issueDesc = document.getElementById("issueDescInput").value;
 	var issueSeverity = document.getElementById("issueSeverityInput").value;
@@ -63,7 +31,7 @@ function saveIssue(e){
 		
 		//If we have something inside the storage, then we will do:
 		var issues = JSON.parse(localStorage.getItem("issues"));
-		issues.push("issue");
+		issues.push(issue);
 		localStorage.setItem("issues", JSON.stringify(issues))
 
 	}
@@ -79,32 +47,6 @@ function saveIssue(e){
 	e.preventDefault();
 }
 
-
-/*function fetchIssues() {
-  var issues = JSON.parse(localStorage.getItem('issues'));
-  var issuesListe = document.getElementById('issuesList');
-
-  issuesList.innerHTML = '';
-
-  for (var i = 0; i < issues.length; i++) {
-    var id = issues[i].id;
-    var desc = issues[i].description;
-    var severity = issues[i].severity;
-    var assignedTo = issues[i].assignedTo;
-    var status = issues[i].status;
-
-    issuesList.innerHTML +=   '<div class="well">'+
-                              '<h6>Issue ID: ' + id + '</h6>'+
-                              '<p><span class="label label-info">' + status + '</span></p>'+
-                              '<h3>' + desc + '</h3>'+
-                              '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '</p>'+
-                              '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
-                              '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a> '+
-                              '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
-                              '</div>';
-  }
-}*/
-
 function fetchIssues(){
 	//fetching issues for a local storage
 	var issues = JSON.parse(localStorage.getItem("issues"));
@@ -113,7 +55,13 @@ function fetchIssues(){
 	issuesList.innerHTML = "";
 
 	for(var i = 0; i < issues.length; i++){
-		var id = issues[i].id;
+
+		// if (id == undefined){
+		// 	console.log("I am here 2");
+		// 	continue;
+		// }
+
+		var id = issues[i].id; console.log("id = " + id);
 		var desc = issues[i].description;
 		var severity = issues[i].severity;
 		var assignedTo = issues[i].assignedTo;
@@ -137,9 +85,60 @@ function fetchIssues(){
 										"<span class='glyphicon glyphicon-user'>" + "</span>" + assignedTo + 
 									"</p>" + 
 
-									"<a href='#' onclick='setStatusClosed(\'"+id+"\')' class='btn btn-warning'> Close </a>" + 
-									"<a href='#' onclick='deleteIssue(\'"+id+"\')' class='btn btn-danger'> Delete </a>" +
-								"</div>";
+									"<a href='#' onclick='setStatusClosed(\""+id+"\")' class='btn btn-warning'> Close </a>" + 
+									
+									"<a href='#' onclick='deleteIssue(\""+id+"\")' class='btn btn-danger'> Delete </a>" +
+							
+								"</div>" + "<br><hr><br>";
 
 	}
 }
+
+
+function setStatusClosed(id){
+	//console.log("I am here11 .... ID = " + id);
+	var issues = JSON.parse(localStorage.getItem("issues"));
+	//debugger;
+	for (var i = 0; i < issues.length; i++){
+		
+		if (issues[i].id == id){
+			issues[i].status = "Closed";
+		}
+	}
+
+	//Change that array(issues) to JSON format
+	localStorage.setItem("issues", JSON.stringify(issues));
+
+	//To update our output on the html, we need to call fetchIssues()
+	fetchIssues();
+
+}
+
+function deleteIssue(id){
+	//console.log("I am here11 .... ID = " + id);
+	var issues = JSON.parse(localStorage.getItem("issues"));
+	
+	for (var i = 0; i < issues.length; i++){
+		
+		if (issues[i].id == id){
+			issues.splice(i, 1);
+		}
+	}
+
+	//Change that array(issues) to JSON format
+	localStorage.setItem("issues", JSON.stringify(issues));
+
+	//To update our output on the html, we need to call fetchIssues()
+	fetchIssues();
+}
+
+
+
+
+
+
+
+
+
+
+
